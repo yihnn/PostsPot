7ecaa15a64cfd30b4e193a5adb52796c60dbd454
yihnn <yihn95@naver.com>
2023-06-08 17:10:46 +0900

    mainHome created

--- src/main/java/com/spring/postspot/PostspotController.java
+++ src/main/java/com/spring/postspot/PostspotController.java
@@ -1,5 +1,20 @@
 package com.spring.postspot;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class PostspotController {
+
+	@GetMapping("/mainHome")
+	public ModelAndView mainHome() {
+		
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("postspot/mainHome");
+		
+		return mav;
+	}
 
 }
