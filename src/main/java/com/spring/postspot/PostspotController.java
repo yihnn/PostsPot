701ce63472a328bbbcf3040f766c3c293dc57b61
yihnn <yihn95@naver.com>
2023-06-14 16:34:43 +0900

    creating account page

--- src/main/java/com/spring/postspot/PostspotController.java
+++ src/main/java/com/spring/postspot/PostspotController.java
@@ -2,6 +2,7 @@ package com.spring.postspot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,5 +17,34 @@ public class PostspotController {
 		
 		return mav;
 	}
+	
+	@GetMapping("/login")
+	public ModelAndView login() {
+		
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("postspot/login");
+		
+		return mav;
+	}
 
+	@PostMapping("/login_ok")
+	public ModelAndView loginOk(){
+		
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("redirect:/mainHome");
+		
+		return mav;
+	}
+	
+	@GetMapping("/createAccount")
+	public ModelAndView createAccount() {
+		
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("postspot/account");
+		
+		return mav;
+	}
 }
